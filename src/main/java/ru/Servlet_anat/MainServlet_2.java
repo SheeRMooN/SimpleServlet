dd99dc47a2e71f2add95cfbbfcca938782d573ab
SheeRMooN <legionerus@yandex.ru>
2020-11-11 17:03:03 +0300

    add

--- src/main/java/ru/Servlet_anat/MainServlet_2.java
+++ src/main/java/ru/Servlet_anat/MainServlet_2.java
@@ -1,5 +1,6 @@
 package ru.Servlet_anat;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,10 +8,37 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/FirstServlet")
+@WebServlet("/Servlet2")
 public class MainServlet_2 extends HttpServlet {
     @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+    }
+
+    @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("Hello");
+        resp.setContentType("text/html;charset=utf8;");
+        System.out.println("Hello ssssss");
+        resp.getWriter().write("<h1>Hello doGet</h1>");
+        String firstName = req.getParameter("firstName");
+        String lastName = req.getParameter("lastName");
+        System.out.println();
+        System.out.println(firstName);
+        System.out.println(lastName);
+        resp.getWriter().write("first name : " + firstName + "\n<br>");
+        resp.getWriter().write("last name : " + lastName + "\n");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        resp.getWriter().write("<h1>Hello doPost</h1>");
+        String firstName = req.getParameter("firstName");
+        String lastName = req.getParameter("lastName");
+        System.out.println();
+        System.out.println(firstName);
+        System.out.println(lastName);
+        resp.getWriter().write("first name : " + firstName + "\n<br>");
+        resp.getWriter().write("last name : " + lastName + "\n");
     }
 }
--- src/main/java/ru/practica/MainServlet.java
+++ src/main/java/ru/practica/MainServlet.java
@@ -1,50 +1,50 @@
-package ru.practica;
-
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.stream.Collectors;
-
-@WebServlet(urlPatterns = {"/cool-servlet"})
-public class MainServlet extends HttpServlet {
-    @Override
-    public void init(ServletConfig config) throws ServletException {
-        super.init(config);
-        log("metod init....!...!...!");
-    }
-
-    @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().write("metod start service....!...!...!\n");
-        super.service(req, resp);
-        resp.getWriter().write("metod close service....!...!...!\n");
-    }
-
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String requestURI = req.getRequestURI();
-        String params = fomatParams(req);
-        resp.getWriter().write("metod doGet....!...!...!\nURI: " + requestURI + "\nParams: \n" + params + "\n");
-    }
-
-    private String fomatParams(HttpServletRequest req) {
-        String params = req.getParameterMap()
-                .entrySet()
-                .stream()
-                .map(entry -> {
-                    String param = String.join(" and ", entry.getValue());
-                    return entry.getKey() + " => " + param;
-                })
-                .collect(Collectors.joining("\n"));
-        return params;
-    }
-
-    @Override
-    public void destroy() {
-        log("metod destroy....!...!...!");
-    }
-}
+//package ru.practica;
+//
+//import javax.servlet.ServletConfig;
+//import javax.servlet.ServletException;
+//import javax.servlet.annotation.WebServlet;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//import java.util.stream.Collectors;
+//
+//@WebServlet(urlPatterns = {"/cool-servlet"})
+//public class MainServlet extends HttpServlet {
+//    @Override
+//    public void init(ServletConfig config) throws ServletException {
+//        super.init(config);
+//        log("metod init....!...!...!");
+//    }
+//
+//    @Override
+//    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        resp.getWriter().write("metod start service....!...!...!\n");
+//        super.service(req, resp);
+//        resp.getWriter().write("metod close service....!...!...!\n");
+//    }
+//
+//    @Override
+//    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        String requestURI = req.getRequestURI();
+//        String params = fomatParams(req);
+//        resp.getWriter().write("metod doGet....!...!...!\nURI: " + requestURI + "\nParams: \n" + params + "\n");
+//    }
+//
+//    private String fomatParams(HttpServletRequest req) {
+//        String params = req.getParameterMap()
+//                .entrySet()
+//                .stream()
+//                .map(entry -> {
+//                    String param = String.join(" and ", entry.getValue());
+//                    return entry.getKey() + " => " + param;
+//                })
+//                .collect(Collectors.joining("\n"));
+//        return params;
+//    }
+//
+//    @Override
+//    public void destroy() {
+//        log("metod destroy....!...!...!");
+//    }
+//}
